d33221b2834f8f1b55e9b79e7881a0a8245ac0d5
cockyb <56942459+cockyb@users.noreply.github.com>
2020-07-13 17:50:33 +0900

    audience board react - board title

--- be/vote/src/main/resources/static/js/chat.js
+++ be/vote/src/main/resources/static/js/chat.js
@@ -1,26 +1,33 @@
 $(document).ready(function() {
-    $(function() {
-        var socket = io("http://localhost:4000");
-        $('#chatform').submit(function(e) {
-            e.preventDefault(); // prevents page reloading
-            socket.emit('chat message', $("#username").text() + " : " +  $('#chatinput').val());
-            $('#chatinput').val('');
-            return false;
+    const socket = io("http://localhost:4000");
+    if($('#username').text()==''){
+        $("#chatinput").attr("disabled",true);
+        $("#chatinput").attr('placeholder', '로그인이 필요합니다.' );
+    } else {
+        $(function() {
+            $("#chatinput").attr("disabled",false);
+            $("#chatinput").attr('placeholder', '내용을 입력하세요.' );
+            $('#chatform').submit(function(e) {
+                e.preventDefault(); // prevents page reloading
+                socket.emit('chat message', $("#username").text() + " : " +  $('#chatinput').val());
+                $('#chatinput').val('');
+                return false;
+            });
+            socket.on('chat message', function(msg) {
+                var username = $("#username").text();
+                var url = msg;
+                const num = url.split(' : ');
+                var param = num[num.length-2];
+    
+                if(username==param)
+                $('#messages').append($('<li id=user>').text(msg));
+                else
+                $('#messages').append($('<li>').text(msg));
+                $("#messages").scrollTop($("#messages")[0].scrollHeight);
+    
+            });
         });
-        socket.on('chat message', function(msg) {
-            var username = $("#username").text();
-            var url = msg;
-            const num = url.split(' : ');
-            var param = num[num.length-2];
-
-            if(username==param)
-            $('#messages').append($('<li id=user>').text(msg));
-            else
-            $('#messages').append($('<li>').text(msg));
-            $("#messages").scrollTop($("#messages")[0].scrollHeight);
-
-        });
-    });
+    }   
     
     $(function() {
         $(".c_h").click(function(e) {
