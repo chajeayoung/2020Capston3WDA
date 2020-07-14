437de9d58c118be791d88ddc6820739aa4543ee7
chajae <60876477+chajeayoung@users.noreply.github.com>
2020-07-14 11:21:49 +0900

    0714/11:21

---
+++ fe/app/riroIndex/managerIndex.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+class ManagerIndex extends React.Component {
+    render() {
+        return (
+            <div>
+                        매니저
+            </div>
+        );
+      }
+}
+
+
+ReactDOM.render(<ManagerIndex/>,document.getElementById('managerIndex'));
---
+++ fe/app/riroIndex/userIndex.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+class UserIndex extends React.Component {
+    render() {
+        return (
+            <div>
+                        일반사용자
+            </div>
+        );
+      }
+}
+
+
+ReactDOM.render(<UserIndex/>,document.getElementById('userIndex'));
